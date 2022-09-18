2459b12761d4ccac6863cc056c3ffe81e979c716
Shubhi Arora <shubhiarora401@gmail.com>
2022-09-18 15:24:03 +0530

    created basic structure

---
+++ app/src/main/java/com/mymusic/AudioModel.java
@@ -0,0 +1,37 @@
+package com.mymusic;
+
+public class AudioModel {
+    String path;
+    String duration;
+    String title;
+
+    public AudioModel(String path, String duration, String title) {
+        this.path = path;
+        this.duration = duration;
+        this.title = title;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ app/src/test/java/com/mymusic/MusicListAdapterTest.java
@@ -0,0 +1,7 @@
+package com.mymusic;
+
+import junit.framework.TestCase;
+
+public class MusicListAdapterTest extends TestCase {
+
+}
